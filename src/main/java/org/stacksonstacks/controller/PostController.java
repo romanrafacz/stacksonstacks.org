8c21e1b5a1fd55b3177859d2efbf2d0334f45fff
shoemonkey <romanr32@gmail.com>
2016-08-08 20:53:16 -0500

    search box added, working, need db link

--- src/main/java/org/stacksonstacks/controller/PostController.java
+++ src/main/java/org/stacksonstacks/controller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.stacksonstacks.domain.Post;
 import org.stacksonstacks.repository.PostRepository;
 
@@ -22,6 +23,12 @@ public class PostController {
 	
 	@Autowired
 	private PostRepository postRepository;
+	
+	@RequestMapping(value="/search", method=RequestMethod.POST)
+	public String searchForm(Model model, String search){
+		model.addAttribute("posts", search);
+		return "post/show_posts_search_test";
+	}
 	
 	@RequestMapping("/")
 	public String listPosts(Model model){
