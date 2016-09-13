9c6b3f8243f35bf2222fb23e8cf3d89cf7fa0eaa
shoemonkey <romanr32@gmail.com>
2016-09-13 12:39:48 -0500

    admin page posts added

--- src/main/java/org/stacksonstacks/controller/AdminController.java
+++ src/main/java/org/stacksonstacks/controller/AdminController.java
@@ -1,15 +1,37 @@
 package org.stacksonstacks.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.stacksonstacks.domain.Post;
+import org.stacksonstacks.repository.PostRepository;
+
 
 @Controller
 public class AdminController {
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@RequestMapping("/admin")
 	public String adminPage(Model model){
 		return "admin/admin_home";
+	}
+	
+	@RequestMapping("/admin/post/new")
+	public String addNewPost(Model model){
+		return "admin/add_post";
+	}
+	
+	@RequestMapping(value="/post/add", method=RequestMethod.POST)
+	public String addNewBlog(@Valid Post post, BindingResult bindingResult, Model model){
+		postRepository.save(post);
+		return "redirect:/post";
 	}
 	
 }
