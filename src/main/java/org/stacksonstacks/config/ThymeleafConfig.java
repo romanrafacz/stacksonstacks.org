8af61ffb824c16021fe10fd95d4ff9aa63640e1e
shoemonkey <romanr32@gmail.com>
2016-10-05 09:03:58 -0500

    resourse handler added to thymelf config

--- src/main/java/org/stacksonstacks/config/ThymeleafConfig.java
+++ src/main/java/org/stacksonstacks/config/ThymeleafConfig.java
@@ -2,6 +2,7 @@ package org.stacksonstacks.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 @Configuration
@@ -10,6 +11,12 @@ public class ThymeleafConfig {
 	@Bean
 	public SpringSecurityDialect springSecurityDialect(){
 		return new SpringSecurityDialect();
+	}
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		if (!registry.hasMappingForPattern("/css/**")){
+			registry.addResourceHandler("/css/**").addResourceLocations("classpath:/");
+		}
 	}
 
 }
