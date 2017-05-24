cf849d8b1b848553ed100b1aca37317bc0e16dc6
rrafacz <romanr32@gmail.com>
2017-05-24 10:31:06 -0500

    Added H2 and Profile Annotations - Dev/Prod

---
+++ src/main/java/org/stacksonstacks/config/DevDataSourceConfig.java
@@ -0,0 +1,30 @@
+package org.stacksonstacks.config;
+
+import javax.sql.DataSource;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Profile("dev")
+public class DevDataSourceConfig {
+	
+	@Bean
+	public DataSource datasource(){
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.build();
+	}
+	
+	@Bean
+	public ServletRegistrationBean h2servletRegistration(){
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		return registration;
+	}
+
+}
