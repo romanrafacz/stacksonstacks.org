0275af9cd2686ba887cbe8d74b8f7166df6e8d57
rrafacz <romanr32@gmail.com>
2017-05-25 12:27:32 -0500

    datasource added test query

--- src/main/java/org/stacksonstacks/config/ProdDataSourceConfig.java
+++ src/main/java/org/stacksonstacks/config/ProdDataSourceConfig.java
@@ -18,7 +18,7 @@ public class ProdDataSourceConfig {
 		HikariConfig hikariConfig = new HikariConfig();
 		//Turned this off, then started working
 		//hikariConfig.setDriverClassName("org.postgresql.Driver");
-		hikariConfig.setJdbcUrl("jdbc:postgresql://192.168.100.128:5432/blog");
+		hikariConfig.setJdbcUrl("jdbc:postgresql://localhost:5432/blog");
 		hikariConfig.setUsername("roman");
 		hikariConfig.setPassword("snickers");
 		
